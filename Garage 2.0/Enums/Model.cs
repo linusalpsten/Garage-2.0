293e7e33751d477d50363a7875d80708528f6f12
Linus Alpsten <linus.alpsten@telia.com>
2017-10-23 09:19:26 +0200

    Add enums

---
+++ Garage 2.0/Enums/Model.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage_2._0.Enums
+{
+    public enum Model
+    {
+    }
+}
--- Garage 2.0/Models/Vehicle.cs
+++ Garage 2.0/Models/Vehicle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Garage_2._0.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,9 @@ namespace Garage_2._0.Models
     {
         public int Id { get; set; }
         public string RegistrationNumber { get; set; }
-        public string Color { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
+        public Color Color { get; set; }
+        public Brand Brand { get; set; }
+        public Model Model { get; set; }
         public int NumberOfWheels { get; set; }
     }
 }
