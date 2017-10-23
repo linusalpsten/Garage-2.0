a2e2e54f768f9ceec62188d8caf033008321a424
Linus Alpsten <linus.alpsten@telia.com>
2017-10-23 10:34:48 +0200

    change validation

--- Garage 2.0/Models/Vehicle.cs
+++ Garage 2.0/Models/Vehicle.cs
@@ -1,6 +1,7 @@
 ﻿using Garage_2._0.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -12,11 +13,13 @@ namespace Garage_2._0.Models
         public int Id { get; set; }
         [Required]
         [RegularExpression(@"[a-zA-Z]{3}\d{3}|[a-zA-Z]{1,8}", ErrorMessage = "Registration number should have 3 letters followed by 3 numbers or 1 to 8 letters")]
+        [DisplayName("Registration Number")]
         public string RegistrationNumber { get; set; }
         public Color Color { get; set; }
         public Brand Brand { get; set; }
         public Model Model { get; set; }
         [Range(1,20,ErrorMessage = "The number of wheels should be between 1 and 20")]
+        [DisplayName("Number of Wheels")]
         public int NumberOfWheels { get; set; }
         public DateTime ParkingTime { get; set; }
     }
