31b54cd2133dfb1357efb13746b33b1b054d83ca
Linus Alpsten <linus.alpsten@telia.com>
2017-10-23 09:16:09 +0200

    Add Vehicle Controller

---
+++ Garage 2.0/DataAccess/VehicleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Garage_2._0.Models;
+
+namespace Garage_2._0.DataAccess
+{
+    public class VehicleContext : DbContext
+    {
+        public VehicleContext() : base("VehicleConnection")
+        {
+
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
