c59180d38227827bf34b3faddad3eb82a42969d1
Linus Alpsten <linus.alpsten@telia.com>
2017-10-23 10:48:02 +0200

    add view models

---
+++ Garage 2.0/ViewModels/VehicleDetails.cs
@@ -0,0 +1,18 @@
+﻿using Garage_2._0.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage_2._0.ViewModels
+{
+    public class VehicleDetails
+    {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public Color Color { get; set; }
+        public Brand Brand { get; set; }
+        public Model Model { get; set; }
+        public DateTime ParkingTime { get; set; }
+    }
+}
---
+++ Garage 2.0/ViewModels/VehicleIndex.cs
@@ -0,0 +1,15 @@
+﻿using Garage_2._0.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage_2._0.ViewModels
+{
+    public class VehicleIndex
+    {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public Model Model { get; set; }
+    }
+}
