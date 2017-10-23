af074097fc71bd1d80011e16b1205364b56de642
Linus Alpsten <linus.alpsten@telia.com>
2017-10-23 09:07:18 +0200

    Add Vehicle model

---
+++ Garage 2.0/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage_2._0.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NumberOfWheels { get; set; }
+    }
+}
