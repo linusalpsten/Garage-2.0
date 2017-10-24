8a29dc847553487d7d05c8e2b68af08495bdf41e
Linus Alpsten <linus.alpsten@telia.com>
2017-10-24 10:29:10 +0200

    add display names

--- Garage 2.0/ViewModels/Statistics.cs
+++ Garage 2.0/ViewModels/Statistics.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Garage_2._0.Enums;
+using System.ComponentModel;
 
 namespace Garage_2._0.ViewModels
 {
     public class Statistics
     {
         public Dictionary<Model, int> VehiclesOfEachType { get; set; }
+        [DisplayName("Total Number of Wheels")]
         public int NrOfWheels { get; set; }
+        [DisplayName("Total Parking Price")]
         public double TotalParkingPrice { get; set; }
         public Dictionary<Brand, int> NrOfEachBrand { get; set; }
     }
