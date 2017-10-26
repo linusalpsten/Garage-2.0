fcbf659936293406a1b10810f963219f1323d04b
Roffe <dvdroffe@hotmail.com>
2017-10-26 11:15:01 +0200

    HEJ

--- Garage 2.0/App_Start/BundleConfig.cs
+++ Garage 2.0/App_Start/BundleConfig.cs
@@ -9,9 +9,7 @@ namespace Garage_2._0
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-latest.js",
-                        "~/Scripts/jquery.tablesorter.min.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,11 +22,13 @@ namespace Garage_2._0
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js",
-                      "~/Scripts/SortOnClick.js"));
+                      "~/Scripts/moment.js",
+                      "~/Scripts/bootstrap-sortable.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-sortable.css"));
         }
     }
 }
--- Garage 2.0/Scripts/SortOnClick.js
+++ Garage 2.0/Scripts/SortOnClick.js
@@ -1,16 +1,12 @@
-﻿//function addOnClick() {
-//    document.getElementById("RegNrLabel").onclick = function () {
-//        document.location.href = document.getElementById("RegNr").href;
-//    }
-//    document.getElementById("ModelLabel").onclick = function () {
-//        document.location.href = document.getElementById("Model").href;
-//    }
-//}
+﻿function addOnClick() {
+    document.getElementById("RegNrLabel").onclick = function () {
+        document.location.href = document.getElementById("RegNr").href;
+    }
+    document.getElementById("ModelLabel").onclick = function () {
+        document.location.href = document.getElementById("Model").href;
+    }
+}
 
-//addOnClick();
+addOnClick();
 
-$(document).ready(function () {
-    // call the tablesorter plugin 
-    $("table").tablesorter({
-    });
-});
+
