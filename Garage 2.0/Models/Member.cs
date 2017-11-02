f4fe72a3acec3bbadf29efd5997b7fe639b98516
Linus Alpsten <linus.alpsten@telia.com>
2017-11-02 09:28:11 +0100

    add member model

---
+++ Garage 2.0/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage_2._0.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
